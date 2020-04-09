a48314e9f6429c3ab179c40d997d04faa56e8ada
jpak3 <jpak@towson.edu>
2020-04-09 13:36:43 -0400

    Add drug listview model
    ViewModels

---
+++ miemss_xamarin/miemss_xamarin/DrugsPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using miemss_xamarin.Models;
+
+namespace miemss_xamarin
+{
+    public class DrugsPageViewModel
+    {
+        public IList<Drug> Drugs { get; private set; }
+
+        public DrugsPageViewModel()
+        {
+            Drugs = DrugData.Drugs;
+        }
+    }
+}
