f17651e72d8a7a993c209bf8b0246a24a079c1df
jpak3 <jpak@towson.edu>
2020-04-09 13:37:49 -0400

    Add Models
    Include Drugs

---
+++ miemss_xamarin/miemss_xamarin/Models/Drug.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miemss_xamarin.Models
+{
+    public class Drug
+    {
+      public string Name { get; set; }
+      public string Details { get; set; }
+    }
+}
---
+++ miemss_xamarin/miemss_xamarin/Models/DrugData.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miemss_xamarin.Models
+{
+    public static class DrugData
+    {
+        public static IList<Drug> Drugs { get; private set; }
+        static DrugData()
+        {
+            Drugs = new List<Drug>();
+
+            Drugs.Add(new Drug
+            {
+                Name = "Acetaminophen",
+                Details = "coming soon"
+            });
+
+            Drugs.Add(new Drug
+            {
+                Name = "Activated Charcoal (Without Sorbitol) ",
+                Details = "coming soon"
+            });
+
+            Drugs.Add(new Drug
+            {
+                Name = "Aspirin",
+                Details = "coming soon"
+            });
+
+            Drugs.Add(new Drug
+            {
+                Name = "Epinephri	",
+                Details = "coming soon"
+            });
+
+            Drugs.Add(new Drug
+            {
+                Name = "Epinephrine Auto-Injector",
+                Details = "coming soon"
+            });
+
+            Drugs.Add(new Drug
+            {
+                Name = "Naloxone",
+                Details = "coming soon"
+            });
+
+            Drugs.Add(new Drug
+            {
+                Name = "Nitroglycerin ",
+                Details = "coming soon"
+            });
+
+            Drugs.Add(new Drug
+            {
+                Name = "Oral Glucose ",
+                Details = "coming soon"
+            });
+
+            Drugs.Add(new Drug
+            {
+                Name = "Oxygen 	 ",
+                Details = "coming soon"
+            });
+
+
+        }
+
+    }
+}
+
+
+
