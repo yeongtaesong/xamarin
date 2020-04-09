c6dae9cfa3ce90f1fb027e0f2995ed68e5277261
jpak3 <jpak@towson.edu>
2020-04-09 13:33:37 -0400

    Update Page2
    add webview - hospital (tentative)

--- miemss_xamarin/miemss_xamarin/Page2.xaml.cs
+++ miemss_xamarin/miemss_xamarin/Page2.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace miemss_xamarin
@@ -15,6 +17,19 @@ namespace miemss_xamarin
         public Page2()
         {
             InitializeComponent();
+
+            //go to initBrowser() function to change html file name
+            initBrowser();
+
+        }
+
+        //Set webview reference to local html file
+        public void initBrowser()
+        {
+            //See IBaseUrl.cs for information on using IBaseUrl interface
+            string path = DependencyService.Get<IBaseUrl>().Get();
+            string url = Path.Combine(path, "HTML/page2_hospital.html");
+            WebView.Source = url;
         }
     }
 }
