0ff38a9dd0af7efc10d34313b472a24d0e1f67b2
jpak3 <jpak@towson.edu>
2020-04-13 09:41:37 -0400

    Updated hospital contact page

---
+++ miemss_xamarin/miemss_xamarin/HospitalContactDetailPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using miemss_xamarin.Models;
+
+
+
+namespace miemss_xamarin
+{
+    public partial class HospitalContactDetailPage : ContentPage
+    {
+        public HospitalContactDetailPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
