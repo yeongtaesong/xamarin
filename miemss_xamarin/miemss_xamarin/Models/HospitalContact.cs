4ea349fb56dd5df58e642dd2ce02fa80529cd027
jpak3 <jpak@towson.edu>
2020-04-13 09:40:30 -0400

    Added hospital contact

---
+++ miemss_xamarin/miemss_xamarin/Models/HospitalContact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miemss_xamarin.Models
+{
+   public  class HospitalContact
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNum { get; set; }
+        public string Website { get; set; }
+        public string Type { get; set; }
+        public string Specialty { get; set; }
+        public string Burn { get; set; }
+        public string Stroke { get; set; }
+    }
+}
---
+++ miemss_xamarin/miemss_xamarin/Models/HospitalContactData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miemss_xamarin.Models
+{
+    public static class HospitalContactData
+    {
+        public static IList<HospitalContact> HospitalContacts { get; private set; }
+        static HospitalContactData()
+
+        {
+            HospitalContacts = new List<HospitalContact>();
+
+            HospitalContacts.Add(new HospitalContact
+            {
+                Name = "Johns Hopkins Bayview Medical Center",
+                Address = "1111 Habor Rd. Baltimore MD",
+                PhoneNum = "111-223-1234",
+                Website = "www.jhu.edu",
+                Type = "Level I Trauma Center",
+                Specialty = "Cardiac  ",
+                Burn = "YES",
+                Stroke = "YES"
+            });
+            HospitalContacts.Add(new HospitalContact
+            {
+                Name = "Johns Hopkins Harbor Medical Center",
+                Address = "",
+                PhoneNum = "",
+                Website = "",
+                Type = "LeveI Trauma Center",
+                Specialty = "  ",
+                Burn = "YES",
+                Stroke = "YES"
+            });
+        }
+    }
+}
