9080dbb4a1441d11761cda6995a212c8ec6cf3e4
jpak3 <jpak@towson.edu>
2020-04-17 00:15:14 -0400

    Added Map/Phone/Website opening funtions

--- miemss_xamarin/miemss_xamarin/HospitalContactDetailPage.xaml.cs
+++ miemss_xamarin/miemss_xamarin/HospitalContactDetailPage.xaml.cs
@@ -20,6 +20,70 @@ namespace miemss_xamarin
 
         }
 
+        async Task ExecuteMapCommand(Location location, string address)
+        {
+            try
+            {
+                var options = new MapLaunchOptions { Name = address };
+                await Map.OpenAsync(location, options);
+            }
+            catch (Exception ex)
+            {
+                ProcessException(ex);
+            }
+        }
+        async Task ExecutePhoneCommand(string phoneNumber)
+        {
+            try
+            {
+                PhoneDialer.Open(phoneNumber);
+            }
+            catch (Exception ex)
+            {
+                ProcessException(ex);
+            }
+        }
+        async Task ExecuteWebCommand(string website)
+        {
+            try
+            {
+                Uri myURL = new Uri(website);
+                await Launcher.OpenAsync(myURL);
+            }
+            catch (Exception ex)
+            {
+                ProcessException(ex);
+            }
+        }
 
+        private async void OnAddressTapGestureRecognizerTapped (object sender, EventArgs e)
+        {
+            var Label = (Label)sender;
+            var locations = await Geocoding.GetLocationsAsync(Label.Text);
+            var location = locations?.FirstOrDefault();
+
+            await ExecuteMapCommand(location, Label.Text);
+        }
+
+        private async void OnPhoneTapGestureRecognizerTapped(object sender, EventArgs e)
+        {
+            var Label = (Label)sender;
+
+            await ExecutePhoneCommand(Label.Text);
+        }
+
+        private async void OnWebTapGestureRecognizerTapped(object sender, EventArgs e)
+        {
+            var Label = (Label)sender;
+            Uri myUri = new Uri(Label.Text); 
+
+            await ExecuteWebCommand(Label.Text);
+        }
+
+        void ProcessException(Exception ex)
+        {
+            if (ex != null)
+                Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+        }
     }
 }
