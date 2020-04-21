01a24d4bb928af1a2fc4225234dac2637331564f
Analilia <analiliaf1992@gmail.com>
2020-04-21 19:30:03 -0400

    Revert "Revert "added assets""
    This reverts commit 3ce34894a5a7e30e424395a74439c73c818c9472.

--- miemss_xamarin/miemss_xamarin/ProtocolsPage.xaml.cs
+++ miemss_xamarin/miemss_xamarin/ProtocolsPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using miemss_xamarin.Models;
 
 namespace miemss_xamarin
 {
@@ -29,6 +30,7 @@ namespace miemss_xamarin
 
         }
 
+        //If header is tapped, list is expanded - finds index value of ResourceGroup
         private void HeaderTapped(object sender, EventArgs e)
         {
             int selectedIndex = _expandedGroups.IndexOf(
@@ -61,17 +63,32 @@ namespace miemss_xamarin
             ProtocolsView.ItemsSource = _expandedGroups;
         }
 
-        //Initialize Page so that only headers are showing. Use ResourceGroup.All to obtain original list.
-        private List<ProtocolGroup> InitializePage(List<ProtocolGroup> List)
+<<<<<<< Updated upstream
+        void OnItemTapped(object sender, ItemTappedEventArgs e)
         {
-            List<ProtocolGroup> InitialList = new List<ProtocolGroup>();
-            foreach (ProtocolGroup protocols in List)
+            ((ListView)sender).SelectedItem = null;
+        }
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var protocol = ((ListView)sender).SelectedItem as Protocol;
+=======
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var  protocol = ((ListView)sender).SelectedItem as Protocol;
+>>>>>>> Stashed changes
+            if (protocol != null)
             {
-                //Only headers are created - no sections will be shown
-                ProtocolGroup temp = new ProtocolGroup(protocols.Heading);
-                InitialList.Add(temp);
+                var page = new DetailedProtocolPage();
+                page.BindingContext = protocol;
+                await Navigation.PushAsync(page);
             }
-            return InitialList;
+<<<<<<< Updated upstream
+=======
+        }
+        void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+>>>>>>> Stashed changes
         }
 
     }
