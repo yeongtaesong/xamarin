67bac44fe9ff890973ea234a1e0b1c97e71ad5f8
Analilia <analiliaf1992@gmail.com>
2020-04-22 12:27:22 -0400

    added list page

---
+++ miemss_xamarin/miemss_xamarin/Models/Resource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miemss_xamarin.Models
+{
+    public class Resource
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ miemss_xamarin/miemss_xamarin/Models/ResourceData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace miemss_xamarin.Models
+{
+    public static class ResourceData
+    {
+        public static IList<Resource> Resources { get; private set; }
+
+        static ResourceData()
+        {
+            Resources = new List<Resource>();
+
+            Resources.Add(new Resource
+            {
+                Name = "CBRNE Resources",
+                Path = "path"
+            });
+
+            Resources.Add(new Resource
+            {
+                Name = "Resource 2",
+                Path = "path"
+            });
+
+            Resources.Add(new Resource
+            {
+                Name = "Resource 3",
+                Path = "path"
+            });
+        }
+
+    }
+}
