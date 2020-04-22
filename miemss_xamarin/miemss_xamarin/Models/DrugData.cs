d604a735d4cfda644f4bd060af5e15c7209b442a
jpak3 <jpak@towson.edu>
2020-04-22 17:46:08 -0400

    Added drug names

--- miemss_xamarin/miemss_xamarin/Models/DrugData.cs
+++ miemss_xamarin/miemss_xamarin/Models/DrugData.cs
@@ -13,68 +13,254 @@ namespace miemss_xamarin.Models
 
             Drugs.Add(new Drug
             {
-                Name = "Acetaminophen",
+                Name = "ALS-Acetaminophen",
                 Details = "Patients ages 2 years and above judged to be in mild to moderate discom-fort (e.g., 2–5 on FACES scale).",
-                Dosage = 0.02
+               
             });
 
             Drugs.Add(new Drug
             {
-                Name = "Activated Charcoal (Without Sorbitol)",
+                Name = "ALS-Activated Charcoal (Without Sorbitol)",
                 Details = "Poisoning by mouth",
-                Dosage = 0.04
+                 
             });
 
             Drugs.Add(new Drug
             {
-                Name = "Aspirin",
+                Name = "ALS-Aspirin",
                 Details = "Chest pain when acute myocardial infarction is suspected.",
-                Dosage = 0.25
+                 
             });
 
             Drugs.Add(new Drug
             {
-                Name = "Epinephrine(1:1,000)",
+                Name = "ALS-Epinephrine(1:1,000)",
                 Details = "Moderate to severe allergic reaction with respiratory distress or mild allergic reaction with history of life-threatening allergic reaction.",
-                Dosage = 0.5
+                 
             });
 
             Drugs.Add(new Drug
             {
-                Name = "Epinephrine Auto-Injector",
+                Name = "ALS-Epinephrine Auto-Injector",
                 Details = "Moderate to severe allergic reaction with respiratory distress or mild allergic reaction with history of life-threatening allergic reaction.",
-                Dosage = 0.94
+                 
                 
             });
 
             Drugs.Add(new Drug
             {
-                Name = "Naloxone",
+                Name = "ALS-Naloxone",
                 Details = "Reverses all effects due to opioid (morphine-like) agents. This drug will reverse the respiratory depression and all central and peripheral nervous system effects.",
-                Dosage = 0.45
+                 
                 
             });
 
             Drugs.Add(new Drug
             {
-                Name = "Nitroglycerin",
+                Name = "ALS-Nitroglycerin",
                 Details = " Patient must have own prescribed sublingual nitroglycerin. Chest pain.",
-                Dosage = 0.78
+                 
             });
 
             Drugs.Add(new Drug
             {
-                Name = "Oral Glucose",
+                Name = "ALS-Oral Glucose",
                 Details = "Altered mental status with known diabetic history. Unconscious for an unknown reason.",
-                Dosage = 0.55
+                 
             });
 
             Drugs.Add(new Drug
             {
-                Name = "Oxygen",
+                Name = "ALS-Oxygen",
                 Details = "Increases oxygen content of the blood. Improves tissue oxygenation. Decreases energy expended for respiratiohn.",
-                Dosage = 0.33
+                 
             });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Acetaminophen",
+                Details = "",
+                 
+            });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Activated Charcoal (Without Sorbitol) ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Adenosine ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Albuterol ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Amiodarone",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Aspirin",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Atropine Sulfate",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Atrovent",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Calcium Chloride",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Dexamethasone ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Dextrose ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Diazepam",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Diphenhydramine Hydrochloride ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Dopamine Hydrochloride",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Epinephrine ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Fentanyl ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS- Glucagon ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Haloperidol (Haldol) ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Ketamine",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Lactated Ringer's ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Lidocaine ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Magnesium Sulfate ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Midazolam ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Morphine Sulfate ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Naloxone ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Nitroglycerin ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Nitroglycerin Paste ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Ondansetron ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS- Oxygen ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Sodium Bicarbonate ",
+                Details = "",
+                 
+                        });
+            Drugs.Add(new Drug
+            {
+                Name = "BLS-Verapamil  ",
+                Details = "",
+                 
+                        });
 
         }
 
