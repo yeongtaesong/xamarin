f33e8856188f434764e8dd1df26559d4a0294b7b
Analilia <analiliaf1992@gmail.com>
2020-04-22 18:24:17 -0400

    fixed bugs

---
+++ miemss_xamarin/miemss_xamarin/NewView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace miemss_xamarin
+{
+    public class NewView : View
+    {
+
+    }
+}
