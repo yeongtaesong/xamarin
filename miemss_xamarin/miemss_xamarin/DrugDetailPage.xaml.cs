031a3231d53b499e00b6bdf02623ef6f032bf570
Analilia <analiliaf1992@gmail.com>
2020-04-26 16:52:16 -0400

    updated conversions

--- miemss_xamarin/miemss_xamarin/DrugDetailPage.xaml.cs
+++ miemss_xamarin/miemss_xamarin/DrugDetailPage.xaml.cs
@@ -20,9 +20,32 @@ namespace miemss_xamarin
 
         private void Button_OnClicked(object sender, EventArgs e)
         {
+            string unit = (string)CalculateButton.BindingContext;
+           if (unit == "lb")
+            {
+                double dosage = Convert.ToDouble(Dosage.Text);
+                double weight = Convert.ToDouble(Weight.Text);
+
+                double calculation = dosage * (weight * 0.45359237);
+               calculation = Math.Round(calculation, 3);
+
+                CalculationLabel.Text = "Calculated dosage: " + calculation.ToString();
+
+            } else if (unit == "kg")
+            {
+                double dosage = Convert.ToDouble(Dosage.Text);
+                double weight = Convert.ToDouble(Weight.Text);
+
+                double calculation = dosage * weight;
+
+                CalculationLabel.Text = "Calculated dosage: " + calculation.ToString();
+            }
+            else
+            {
+                DisplayAlert("Message", "Please select a unit.", "ok");
+            }
 
 
-            
         }
         private void Calculator_OnClicked(object sender, EventArgs e)
         {
