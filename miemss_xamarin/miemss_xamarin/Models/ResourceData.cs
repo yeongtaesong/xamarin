5e8fa105567ac8f67922546436cec2d1130ec4c0
Analilia <analiliaf1992@gmail.com>
2020-05-06 12:40:38 -0400

    fixed resource link

--- miemss_xamarin/miemss_xamarin/Models/ResourceData.cs
+++ miemss_xamarin/miemss_xamarin/Models/ResourceData.cs
@@ -16,7 +16,7 @@ namespace miemss_xamarin.Models
             Resources.Add(new Resource
             {
                 Name = "MIEMSS-Infectious Diseases Program",
-                Path = "http://www.miemss.org/home/infectious-diseases",
+                Path = "https://www.miemss.org/home/infectious-diseases",
                 IsWebView = true
             });
 
